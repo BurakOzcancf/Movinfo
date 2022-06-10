600e3342c3ebb1e220a7ea4e8d51a005cceeb77c
unknown <burakozcancf@gmail.com>
2022-06-10 19:57:42 +0200

    bookmark sorted

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 import Category from "./Category";
-const Bookmarks = () => {
-  const bookmarks = useSelector((state) => state.bookmark.bookmarks);
-  console.log(bookmarks);
+const mapStateToProps = (state) => ({
+  movie: state.bookmark.favMovie,
+  serie: state.bookmark.favSerie,
+  person: state.bookmark.favPerson,
+});
+const Bookmarks = ({ movie, serie, person }) => {
   return (
     <div>
-      <Category heading={"Bookmarks"} movies={bookmarks} />
-      {!bookmarks[0] && (
+      <h1 className="text-5xl text-center p-4">Bookmarks</h1>
+      {!movie[0] && !serie[0] && !person[0] && (
         <h2 className="text-3xl text-center p-4">Upps, here is empty!</h2>
       )}
+      <Category
+        type={"movie"}
+        category={movie}
+        heading={movie[0] && "Movies"}
+        movies={movie}
+      />
+      <Category
+        type={"tv"}
+        category={serie}
+        heading={serie[0] && "Series"}
+        movies={serie}
+      />
+      <Category
+        category={person}
+        heading={person[0] && "People"}
+        movies={person}
+      />
     </div>
   );
 };
 
-export default Bookmarks;
+export default connect(mapStateToProps)(Bookmarks);
