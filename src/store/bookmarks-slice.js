27176e74e11330a6093ae85510be46b59c48da2b
unknown <burakozcancf@gmail.com>
2022-06-10 19:57:09 +0200

    redux updated

--- src/store/bookmarks-slice.js
+++ src/store/bookmarks-slice.js
@@ -4,6 +4,9 @@ const bookmarksSlice = createSlice({
   name: "bookmark",
   initialState: {
     bookmarks: [],
+    favMovie: [],
+    favSerie: [],
+    favPerson: [],
   },
   reducers: {
     addBookmarks: (state, action) => {
@@ -18,7 +21,44 @@ const bookmarksSlice = createSlice({
         );
       }
     },
+    addFavMovie: (state, action) => {
+      const itemIndex = state.favMovie.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex < 0) {
+        state.favMovie = [...state.favMovie, action.payload];
+      } else {
+        state.favMovie = state.favMovie.filter(
+          (fav) => fav.id !== action.payload.id
+        );
+      }
+    },
+    addFavSerie: (state, action) => {
+      const itemIndex = state.favSerie.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex < 0) {
+        state.favSerie = [...state.favSerie, action.payload];
+      } else {
+        state.favSerie = state.favSerie.filter(
+          (fav) => fav.id !== action.payload.id
+        );
+      }
+    },
+    addFavPerson: (state, action) => {
+      const itemIndex = state.favPerson.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex < 0) {
+        state.favPerson = [...state.favPerson, action.payload];
+      } else {
+        state.favPerson = state.favPerson.filter(
+          (fav) => fav.id !== action.payload.id
+        );
+      }
+    },
   },
 });
-export const { addBookmarks } = bookmarksSlice.actions;
+export const { addBookmarks, addFavMovie, addFavPerson, addFavSerie } =
+  bookmarksSlice.actions;
 export default bookmarksSlice.reducer;
